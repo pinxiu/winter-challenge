c1cf3e11d8096aa61a8feb3416e7f506bd3c9c90
Alex Kong <alexkong@Alexs-MacBook-Air.local>
2016-12-25 18:54:04 -0800

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161225204653) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "mission_type"
     t.datetime "created_at",   null: false
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 20161225204653) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "mission_id"
-    t.index ["mission_id"], name: "index_comments_on_mission_id"
+    t.index ["mission_id"], name: "index_comments_on_mission_id", using: :btree
   end
 
   create_table "missions", force: :cascade do |t|
@@ -34,7 +37,9 @@ ActiveRecord::Schema.define(version: 20161225204653) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.index ["category_id"], name: "index_missions_on_category_id"
+    t.index ["category_id"], name: "index_missions_on_category_id", using: :btree
   end
 
+  add_foreign_key "comments", "missions"
+  add_foreign_key "missions", "categories"
 end
