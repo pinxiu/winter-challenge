3097cddfe235168afda91d99121cf02609f2b0e5
Alex Kong <alexkong@Alexs-MacBook-Air.local>
2016-12-25 19:02:10 -0800

    remove unused filter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,4 @@
 class CommentsController < ApplicationController
-  :before_filter :merge_params, :only => :create
-
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
@@ -16,11 +14,5 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:name, :team, :mission_id, :content)
-    end
-
-    def merge_params
-        params[:comment].merge!{
-          
-        }
     end
 end
