3f224ce3ad4562f3db9b20732c8f187953c9121b
Alex Kong <alexkong@Alexs-MacBook-Air.local>
2016-12-25 19:06:12 -0800

    actually use gem instead

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem 'bootstrap-sass', '~> 3.2.0'
+gem 'rails_12factor', group: :production
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.serve_static_assets = true
-
 
 module KoinjrWinterchallenge
   class Application < Rails::Application
