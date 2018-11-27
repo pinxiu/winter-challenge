69a2b5e88b2634c2d102d1f32751869722e546db
Kimberley Vanalstine <kimberley.vanalstine@moat.com>
2018-11-27 14:37:13 -0800

    Removed unnecessary challenges category

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -37,13 +37,7 @@ class StaticPagesController < ApplicationController
       @winner = @points.delete_at(0)
 	def new
 	 @comment = Comment.new
-	end
-
-      @sg = Mission.where(:category_id => Category.where(:mission_type => "Spiritual Growth").first.id)
-      @evangelism = Mission.where(:category_id => Category.where(:mission_type => "Evangelism").first.id)
-      @ser = Mission.where(:category_id => Category.where(:mission_type => "Service/Gratitude").first.id)
-      @eq = Mission.where(:category_id => Category.where(:mission_type => "Equipping").first.id)
-   end
+	end	
 
    def challenges
    	@sg = Mission.where(:category_id => Category.where(:mission_type => "Spiritual Growth").first.id)
