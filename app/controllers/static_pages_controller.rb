2acc5aee02e73f8b8ae27c44b3babdc90df30b19
Kimberley Vanalstine <kimberley.vanalstine@moat.com>
2018-11-28 14:50:40 -0800

    Retrying old Page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,24 +1,19 @@
 class StaticPagesController < ApplicationController
    def home
       @totalCount = Comment.count
-      one = Comment.where(:team => "A2F Bros").all
-      two = Comment.where(:team => "A2F Sis").all
-      three = Comment.where(:team => "Klesis Bros").all
-      four = Comment.where(:team => "Klesis Sis").all
-      five = Comment.where(:team => "MakeNew Bros").all
-      six = Comment.where(:team => "MakeNew Sis").all
+   	one = Comment.where(:team => "KJ HG Girls").all
+      two = Comment.where(:team => "KJ HG Guys").all
+      three = Comment.where(:team => "JI/BE HG Girls").all
+      four = Comment.where(:team => "JI/BE HG Guys").all
    	@oneSum = 0;
    	@twoSum = 0;
-	@threeSum = 0;
-	@fourSum = 0;
-	@fiveSum = 0;
-	@sixSum = 0;
-    
-       
-   	  one.each do |post|
-   		 @oneSum += post.mission.points
-      end
-       
+      @threeSum = 0;
+      @fourSum = 0;
+
+   	one.each do |post|
+   		@oneSum += post.mission.points
+   	end
+
       two.each do |post|
          @twoSum += post.mission.points
       end
@@ -30,26 +25,19 @@ class StaticPagesController < ApplicationController
       four.each do |post|
          @fourSum += post.mission.points
       end
-       
-      five.each do |post|
-         @fiveSum += post.mission.points
-      end       
-    
-      six.each do |post|
-         @sixSum += post.mission.points
-      end
-      @points = [['A2F Bros', @oneSum], ['A2F Sis', @twoSum], ['Klesis Bros', @threeSum], ['Klesis Sis', @fourSum], ['MakeNew Bros', @fiveSum], ['MakeNew Sis', @sixSum]].sort do |a, b|
+
+      @points = [['KJ HG Girls', @oneSum], ['KJ HG Guys', @twoSum], ['JI/BE HG Girls', @threeSum], ['JI/BE HG Guys', @fourSum]].sort do |a, b|
          b[1] <=> a[1]
       end
 
       @winner = @points.delete_at(0)
-	 @comment = Comment.new
-       
-    @sg = Mission.where(:category_id => Category.where(:mission_type => "Spiritual Growth").first.id)
-   	@evangelism = Mission.where(:category_id => Category.where(:mission_type => "Evangelism").first.id)
-   	@service = Mission.where(:category_id => Category.where(:mission_type => "Service/Gratitude").first.id)
-   	@equip = Mission.where(:category_id => Category.where(:mission_type => "Equipping").first.id)
 
+   	@comment = Comment.new
+
+      @sg = Mission.where(:category_id => Category.where(:mission_type => "Spiritual Growth").first.id)
+      @evangelism = Mission.where(:category_id => Category.where(:mission_type => "Evangelism").first.id)
+      @ser = Mission.where(:category_id => Category.where(:mission_type => "Service/Gratitude").first.id)
+      @eq = Mission.where(:category_id => Category.where(:mission_type => "Equipping").first.id)
    end
 
    def challenges
@@ -60,27 +48,20 @@ class StaticPagesController < ApplicationController
    end
 
    def stats
-      #@one = []
-      #@two = []
-      #@three = []
-      #@four = []
-      #@five = []
-      #@six = []
-
+      #@one = ['Kevan', 'Jon Kim', 'Will', 'Alex', 'Josh Joo', 'Kevin Stock', 'Daniel Liu', 'Godwin Law', 'Jonathan Chen']
+      #@two = ['Jenny', 'Kat Kim', 'Alex', 'Adeline', 'Huizhen', 'Dasol', 'Vivian L', 'Jennifer Li', 'Stella Oh', 'Michelle Kim', 'Vanessa', 'Mira Chiu', 'Katherine Cai']
+      #@three = ['Joe', 'Brian Jue', 'Ivan Yung', 'Jeff W', 'Nathan M', 'Andrew N.', 'Josh Kim', 'David Lee', 'Daniel Shan', 'Matt Estrada', 'Ben VDH', 'Noah Kang']
+      #@four = ['Irene', 'Ellen Jue', 'San Yung', 'Joyce Cho', 'Joyce Han', 'Nancy P.', 'Claire Lee', 'Kristy J', 'Grace Park', 'Yvonne W', 'Micaela W']
 
       @totalCount = Comment.count
-      one = Comment.where(:team => "A2F Bros").all
-      two = Comment.where(:team => "A2F Sis").all
-      three = Comment.where(:team => "Klesis Bros").all
-      four = Comment.where(:team => "Klesis Sis").all
-      five = Comment.where(:team => "MakeNew Bros").all
-      six = Comment.where(:team => "MakeNew Sis").all
+      one = Comment.where(:team => "KJ HG Girls").all
+      two = Comment.where(:team => "KJ HG Guys").all
+      three = Comment.where(:team => "JI/BE HG Girls").all
+      four = Comment.where(:team => "JI/BE HG Guys").all
       @oneSum = 0;
       @twoSum = 0;
       @threeSum = 0;
       @fourSum = 0;
-      @fiveSum = 0;
-      @sixSum = 0;
 
       one.each do |post|
          @oneSum += post.mission.points
@@ -98,15 +79,7 @@ class StaticPagesController < ApplicationController
          @fourSum += post.mission.points
       end
 
-      five.each do |post|
-         @fiveSum += post.mission.points
-      end
-
-      six.each do |post|
-         @sixSum += post.mission.points
-      end
-
-      @points = [['A2F Bros', @oneSum], ['A2F Sis', @twoSum], ['Klesis Bros', @threeSum], ['Klesis Sis', @fourSum], ['MakeNew Bros', @fiveSum], ['MakeNew Sis', @sixSum]].sort do |a, b|
+      @points = [['KJ HG Girls', @oneSum], ['KJ HG Guys', @twoSum], ['JI/BE HG Girls', @threeSum], ['JI/BE HG Guys', @fourSum]].sort do |a, b|
          b[1] <=> a[1]
       end
 
