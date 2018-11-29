f64a191d927c8fa9a3316bde75c0e91ac19c7c0b
Kimberley Vanalstine <kimberley.vanalstine@moat.com>
2018-11-28 16:46:32 -0800

    changed assets'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,15 +75,12 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-  
-  # Serve static assets
-  config.serve_static_assets = true
-  config.public_file_server.enabled = true
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
