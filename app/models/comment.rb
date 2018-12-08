90e1a781bce17bb34d91d3a13e04a3457b813aa2
Kimberley Vanalstine <kimberley.vanalstine@moat.com>
2018-12-08 11:17:09 -0800

    Updated Challenges on Site and Changed Code for s3

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,6 +12,8 @@ class Comment < ApplicationRecord
 
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
 	:url  => ":s3_domain_url",
+    :s3_region: ENV.fetch("AWS_REGION"),
+    :s3_host_name => ENV["S3_HOST_NAME"],
     :path => "public/avatars/:id/:style_:basename.:extension",
     :storage => :fog,
     :fog_credentials => {
@@ -24,6 +26,8 @@ class Comment < ApplicationRecord
     has_attached_file :video, styles: {:thumb => ["400x400#", :jpg]},  
     :processors => [:transcoder],
     :url  => ":s3_domain_url",
+    :s3_region: ENV.fetch("AWS_REGION"),
+    :s3_host_name => ENV["S3_HOST_NAME"],
     :path => "public/videos/:id/:style_:basename.:extension",
     :storage => :fog,
     :fog_credentials => {
