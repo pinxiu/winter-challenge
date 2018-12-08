2b0589c2ea6de0bb931dbd8761c85c43dcd3b6f1
Kimberley Vanalstine <kimberley.vanalstine@moat.com>
2018-12-08 11:22:36 -0800

    Slight change to s3 code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,7 +12,7 @@ class Comment < ApplicationRecord
 
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" },
 	:url  => ":s3_domain_url",
-    :s3_region: ENV.fetch("AWS_REGION"),
+    :s3_region => ENV.fetch("AWS_REGION"),
     :s3_host_name => ENV["S3_HOST_NAME"],
     :path => "public/avatars/:id/:style_:basename.:extension",
     :storage => :fog,
