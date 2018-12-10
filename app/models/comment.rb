183d65f262a68afb67f0ec96c78695fecde3df8f
Kimberley Vanalstine <kimberley.vanalstine@moat.com>
2018-12-10 11:35:32 -0800

    now added change

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,7 +23,7 @@ class Comment < ApplicationRecord
     },
     fog_directory: ENV["FOG_DIRECTORY"]
 
-    has_attached_file :video, styles: {:medium => { :geometry => "640x480", :format => 'flv' },
+    has_attached_file :video, styles: {:medium => { :geometry => "640x480", :format => 'mp4' },
     :thumb => { :geometry => "400x400#", :format => 'jpg', :time => 10 }},  
     :processors => [:transcoder],
     :url  => ":s3_domain_url",
