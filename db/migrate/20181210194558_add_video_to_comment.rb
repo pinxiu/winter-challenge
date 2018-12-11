046cb244dd35baae046b24bcbecd8dec246038ca
Kimberley Vanalstine <kimberley.vanalstine@moat.com>
2018-12-11 10:11:35 -0800

    Deleted migration file

--- db/migrate/20181210194558_add_video_to_comment.rb
+++
@@ -1,9 +0,0 @@
-class AddVideoToComment < ActiveRecord::Migration[5.0]
-   def self.up
-    add_attachment :comments, :video
-  end
-
-  def self.down
-    remove_attachment :comments, :video
-  end
-end
