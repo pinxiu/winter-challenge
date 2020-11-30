88f637adc09c00470a1ce0ec138f0407ee026687
Phoebe Gong <pinxiu.gong@gmail.com>
2020-11-29 16:30:36 -0800

    use us-west-1 region in fog

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,7 +20,8 @@ class Comment < ApplicationRecord
     :fog_credentials => {
         provider: 'AWS',
         aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        region: 'us-west-1'
     },
     fog_directory: ENV["FOG_DIRECTORY"]
 
