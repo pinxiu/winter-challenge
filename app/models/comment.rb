022928f175e6babccb6b35cfb650be473e4f6676
Phoebe Gong <pinxiu.gong@gmail.com>
2020-12-05 20:17:53 -0800

    fix typo, and remove extra config

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -21,7 +21,7 @@ class Comment < ApplicationRecord
         provider: 'AWS',
         aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
         aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-        region: ENV["AWS_REGION"],
+        region: ENV["AWS_REGION"]
     },
     fog_directory: ENV["FOG_DIRECTORY"]
 
@@ -43,7 +43,7 @@ class Comment < ApplicationRecord
         provider: 'AWS',
         aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
         aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-        region: ENV["AWS_REGION"],
+        region: ENV["AWS_REGION"]
     },
     fog_directory: ENV["FOG_DIRECTORY"]
 
