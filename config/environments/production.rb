c6f0cc8c2fbbf5757fb840d3c70ac74bfa49b1b4
atkwok <atkwok@berkeley.edu>
2020-12-05 20:12:18 -0800

    fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,12 +76,13 @@ Rails.application.configure do
   
   #fix video upload by setting AWS path?
   config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_region => ENV['AWS_REGION'],
-      :s3_credentials => {
-        :bucket => ENV['AWS_BUCKET'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_region => ENV['AWS_REGION'],
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
 
 
