bd6da5537c5d1b38c4ac3c550b5012bf92b02e07
Phoebe Gong <pinxiu.gong@gmail.com>
2020-12-05 19:15:17 -0800

    add team info

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -42,7 +42,7 @@ class StaticPagesController < ApplicationController
         end
     end
 
-      @points = [['Brothers', @oneSum], ['Sisters', @twoSum]].sort do |a, b|
+      @points = [['Team Jeff', @oneSum], ['Team Jisup', @twoSum], ['Team Gary', @threeSum], ['Team Adam', @fourSum], ['Team Sara', @fiveSum], ['Team Ella', @sixSum], ['Team Lynn', @sevenSum]].sort do |a, b|
          b[1] <=> a[1]
       end
 
@@ -68,15 +68,22 @@ class StaticPagesController < ApplicationController
       @challengepmt = Mission.where(:category_id => Category.where(:mission_type => "Message / Talk").first.id)
    end
 
-   def stats
-      #@one = ['Kevan', 'Jon Kim', 'Will', 'Alex', 'Josh Joo', 'Kevin Stock', 'Daniel Liu', 'Godwin Law', 'Jonathan Chen']
-      #@two = ['Jenny', 'Kat Kim', 'Alex', 'Adeline', 'Huizhen', 'Dasol', 'Vivian L', 'Jennifer Li', 'Stella Oh', 'Michelle Kim', 'Vanessa', 'Mira Chiu', 'Katherine Cai']
-      
+   def stats   
       @totalCount = Comment.count
-      one = Comment.where(:team => "Brothers").all
-      two = Comment.where(:team => "Sisters").all
+      one = Comment.where(:team => "Team Jeff").all
+      two = Comment.where(:team => "Team Jisup").all
+      three = Comment.where(:team => "Team Gary").all
+      four = Comment.where(:team => "Team Adam").all
+      five = Comment.where(:team => "Team Sara").all
+      six = Comment.where(:team => "Team Ella").all
+      seven = Comment.where(:team => "Team Lynn").all
       @oneSum = 0;
       @twoSum = 0;
+      @threeSum = 0;
+      @fourSum = 0;
+      @fiveSum = 0;
+      @sixSum = 0;
+      @sevenSum = 0;
 
    	one.each do |post|
         @multiply = begin
@@ -111,9 +118,94 @@ class StaticPagesController < ApplicationController
             @twoSum += 0 * @multiply
         end
     end
+
+    three.each do |post|
+        @multiply = begin
+            Integer(post.multiplier)
+        rescue
+            1
+        end
+        
+      @threeSum += post.mission.points  * @multiply
+        if post.is_type_of_video? 
+            @threeSum += 10 * @multiply
+        elsif post.is_type_of_image?
+            @threeSum += 5 * @multiply
+        else 
+            @threeSum += 0 * @multiply
+        end
+    end
+
+    four.each do |post|
+        @multiply = begin
+            Integer(post.multiplier)
+        rescue
+            1
+        end
+        
+        @fourSum += post.mission.points  * @multiply
+        if post.is_type_of_video? 
+            @fourSum += 10 * @multiply
+        elsif post.is_type_of_image?
+            @fourSum += 5 * @multiply
+        else 
+            @fourSum += 0 * @multiply
+        end
+    end
+
+    five.each do |post|
+        @multiply = begin
+            Integer(post.multiplier)
+        rescue
+            1
+        end
+        
+      @fiveSum += post.mission.points  * @multiply
+        if post.is_type_of_video? 
+            @fiveSum += 10 * @multiply
+        elsif post.is_type_of_image?
+            @fiveSum += 5 * @multiply
+        else 
+            @fiveSum += 0 * @multiply
+        end
+    end
+
+    six.each do |post|
+        @multiply = begin
+            Integer(post.multiplier)
+        rescue
+            1
+        end
+        
+        @sixSum += post.mission.points  * @multiply
+        if post.is_type_of_video? 
+            @sixSum += 10 * @multiply
+        elsif post.is_type_of_image?
+            @sixSum += 5 * @multiply
+        else 
+            @sixSum += 0 * @multiply
+        end
+    end
+
+    seven.each do |post|
+        @multiply = begin
+            Integer(post.multiplier)
+        rescue
+            1
+        end
+        
+      @sevenSum += post.mission.points  * @multiply
+        if post.is_type_of_video? 
+            @sevenSum += 10 * @multiply
+        elsif post.is_type_of_image?
+            @sevenSum += 5 * @multiply
+        else 
+            @sevenSum += 0 * @multiply
+        end
+    end
        
 
-      @points = [['Brothers', @oneSum], ['Sisters', @twoSum]].sort do |a, b|
+      @points = [['Team Jeff', @oneSum], ['Team Jisup', @twoSum], ['Team Gary', @threeSum], ['Team Adam', @fourSum], ['Team Sara', @fiveSum], ['Team Ella', @sixSum], ['Team Lynn', @sevenSum]].sort do |a, b|
          b[1] <=> a[1]
       end
 
@@ -123,9 +215,19 @@ class StaticPagesController < ApplicationController
        
        @oneStat = @oneSum / 20
        @twoStat = @twoSum / 20
+       @threeStat = @threeSum / 20
+       @fourStat = @fourSum / 20
+       @fiveStat = @fiveSum / 20
+       @sixStat = @sixSum / 20
+       @sevenStat = @sevenSum / 20
        
        @barOne = @oneStat + 5
        @barTwo = @twoStat + 5
+       @barThree = @threeStat + 5
+       @barFour = @fourStat + 5
+       @barFive = @fiveStat + 5
+       @barSix = @sixStat + 5
+       @barSeven = @sevenStat + 5
        
    end
 
