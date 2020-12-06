217bb12cbb847bf6d47d57cd9bff14cb21b93ea0
Phoebe Gong <pinxiu.gong@gmail.com>
2020-12-05 18:19:05 -0800

    revert table deletion order

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.delete_all
 Mission.delete_all
+Category.delete_all
 
 dbr = Category.create!(mission_type: 'Bible reading')
 dwp = Category.create!(mission_type: 'Prayer')
