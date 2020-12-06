c9d01c77a50e85816554b8792f14e0100a0c525d
Phoebe Gong <pinxiu.gong@gmail.com>
2020-12-05 20:20:18 -0800

    ahhhh another comma

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -42,7 +42,7 @@ class Comment < ApplicationRecord
     :fog_credentials => {
         provider: 'AWS',
         aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
         region: ENV["AWS_REGION"]
     },
     fog_directory: ENV["FOG_DIRECTORY"]
