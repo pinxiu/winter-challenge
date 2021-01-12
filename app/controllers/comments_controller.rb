e564be971d34b88c71ab2268e9cea56d53dd65f9
atkwok <atkwok@berkeley.edu>
2021-01-11 21:34:57 -0800

    added triple points for bible reading

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,17 @@ class CommentsController < ApplicationController
     comment = Comment.new(comment_params)
     comment.team = Player.where(:name => comment.name).first.team.name
     comment.student_multiplier = 1
-    if ['Alvin Tan', 'Rick Zhong', 'Sean Zhang', 'Kevin Lee', 'Youchen (Victor) Zhang', 'Jenny Huang', 'Jacklyn Ang'].include?(comment.name)
+
+    # puts comment.mission.name
+    # puts comment.mission_id
+
+    if comment.mission_id == 50
+      # Not actually student multiplier, just point multiplier for bible reading
+      comment.student_multiplier = 3
+    elsif ['Alvin Tan', 'Rick Zhong', 'Sean Zhang', 'Kevin Lee', 'Youchen (Victor) Zhang', 'Jenny Huang', 'Jacklyn Ang'].include?(comment.name)
       comment.student_multiplier = 2
     end
+
     if comment.save
       flash[:success] = "Comment posted! Your progress has been logged."
       redirect_to root_url
